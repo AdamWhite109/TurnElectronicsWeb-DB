e9b2ae29c63640ec3fefa985a6c62ed6b8934637
Adam White <44016026+AdamWhite109@users.noreply.github.com>
2019-11-01 12:33:23 -0700

    TE page 11/1
    Figure out styling.

---
+++ accountmanager/home.js
@@ -0,0 +1,48 @@
+// Functions populating the different links
+
+// find products
+function popluateProducts() {
+	// get name
+	// name --> database product type
+	// possible names (home: topProducts, repairs&upgrades: repairs and )
+	// get json file of list of products base on type
+	// loop through the list of products and assign it to cards
+}
+
+function wip_body() {
+	// wip entire body of html page
+}
+
+function how_to_videos() {
+	// reference wip_body function
+	// populate with chosen videos
+}
+
+function faq() {
+	// reference wip_body function
+	// popluate with faq
+}
+
+// search function
+function searchProducts() {
+
+}
+
+// filter function - I don't know if I want this one.
+function filter() {
+	// find which make
+	// of current products only loop through that make
+}
+
+function add_to_cart() {
+	// get the shopping cart id (computer or account)
+	// update database to have the item in that account id
+}
+
+
+// I do not know if this is necessary on simply reference a new page
+function product_page() {
+	// call wip_body function
+	// get product id or item id
+	//
+}
